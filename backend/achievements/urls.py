318b6c83ae225e78e14f05e8b13df4ea734eac71
ZenChris <121231422+ZenChris18@users.noreply.github.com>
2025-04-16 00:25:02 +0800

    Badge now updates immediately in the header added new api calls

--- backend/achievements/urls.py
+++ backend/achievements/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import get_total_points, get_badges, get_unlocked_badges
+from .views import get_total_points, get_badges, get_unlocked_badges, get_selected_badge, set_selected_badge
 
 urlpatterns = [
     path("total-points/", get_total_points, name="total_points"),
     path("badges/", get_badges, name="badges"),
     path("unlocked-badges/", get_unlocked_badges, name="unlocked_badges"),
+    path("select-badge/", set_selected_badge, name="select_badge"),
+    path("selected-badge/", get_selected_badge, name="selected_badge"),
+
 ]
